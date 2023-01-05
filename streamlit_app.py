b5affa92cd0eb18f5cda9271256470d97a0b890b
Rafael <rafaelvleite@icloud.com>
2023-01-04 21:08:30 -0300

    layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,15 @@ st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?l
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('ETHXP Dashboards')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('Relevant dashboards for better insights')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('', 'https://youtube.com/dataprofessor', 'Deribit Historic Data for ETH Options: 24 Hours Contracts', icon_size)
+st_button('', 'https://youtube.com/codingprofessor', 'Deribit Historic Data for ETH Options: 48 Hours Contracts', icon_size)
+st_button('', 'https://data-professor.medium.com/', 'Deribit Historic Data for ETH Options: Friday Contracts', icon_size)
+st_button('', 'https://twitter.com/thedataprof/', 'Deribit Historic Data for ETH Options: Monthly Contracts', icon_size)
+st_button('', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Deribit Historic Data for ETH Options: Quarterly Contracts', icon_size)
+st_button('', 'https://sendfox.com/dataprofessor/', 'Deribit Historic Data for ETH Options: Semesterly Contracts', icon_size)
